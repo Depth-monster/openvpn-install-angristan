1ad430dea907e171deb3a8488e3e0edb75456069
Azat Hajyyev <122405130+Depth-monster@users.noreply.github.com>
2024-10-27 12:41:49 +0300

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -1059,105 +1059,6 @@ scramble xormask 5" >>/etc/openvpn/client-template.txt
 	echo "If you want to add more clients, you simply need to run this script another time!"
 }
 
-function newClient() {
-	echo ""
-	echo "Tell me a name for the client."
-	echo "The name must consist of alphanumeric character. It may also include an underscore or a dash."
-
-	until [[ $CLIENT =~ ^[a-zA-Z0-9_-]+$ ]]; do
-		read -rp "Client name: " -e CLIENT
-	done
-
-	echo ""
-	echo "Do you want to protect the configuration file with a password?"
-	echo "(e.g. encrypt the private key with a password)"
-	echo "   1) Add a passwordless client"
-	echo "   2) Use a password for the client"
-
-	until [[ $PASS =~ ^[1-2]$ ]]; do
-		read -rp "Select an option [1-2]: " -e -i 1 PASS
-	done
-
-	CLIENTEXISTS=$(tail -n +2 /etc/openvpn/easy-rsa/pki/index.txt | grep -c -E "/CN=$CLIENT\$")
-	if [[ $CLIENTEXISTS == '1' ]]; then
-		echo ""
-		echo "The specified client CN was already found in easy-rsa, please choose another name."
-		exit
-	else
-		cd /etc/openvpn/easy-rsa/ || return
-		case $PASS in
-		1)
-			EASYRSA_CERT_EXPIRE=3650 ./easyrsa --batch build-client-full "$CLIENT" nopass
-			;;
-		2)
-			echo "⚠️ You will be asked for the client password below ⚠️"
-			EASYRSA_CERT_EXPIRE=3650 ./easyrsa --batch build-client-full "$CLIENT"
-			;;
-		esac
-		echo "Client $CLIENT added."
-	fi
-
-	# Home directory of the user, where the client configuration will be written
-	if [ -e "/home/${CLIENT}" ]; then
-		# if $1 is a user name
-		homeDir="/home/${CLIENT}"
-	elif [ "${SUDO_USER}" ]; then
-		# if not, use SUDO_USER
-		if [ "${SUDO_USER}" == "root" ]; then
-			# If running sudo as root
-			homeDir="/root"
-		else
-			homeDir="/home/${SUDO_USER}"
-		fi
-	else
-		# if not SUDO_USER, use /root
-		homeDir="/root"
-	fi
-
-	# Determine if we use tls-auth or tls-crypt
-	if grep -qs "^tls-crypt" /etc/openvpn/server.conf; then
-		TLS_SIG="1"
-	elif grep -qs "^tls-auth" /etc/openvpn/server.conf; then
-		TLS_SIG="2"
-	fi
-
-	# Generates the custom client.ovpn
-	cp /etc/openvpn/client-template.txt "$homeDir/$CLIENT.ovpn"
-	{
-		echo "<ca>"
-		cat "/etc/openvpn/easy-rsa/pki/ca.crt"
-		echo "</ca>"
-
-		echo "<cert>"
-		awk '/BEGIN/,/END CERTIFICATE/' "/etc/openvpn/easy-rsa/pki/issued/$CLIENT.crt"
-		echo "</cert>"
-
-		echo "<key>"
-		cat "/etc/openvpn/easy-rsa/pki/private/$CLIENT.key"
-		echo "</key>"
-
-		case $TLS_SIG in
-		1)
-			echo "<tls-crypt>"
-			cat /etc/openvpn/tls-crypt.key
-			echo "</tls-crypt>"
-			;;
-		2)
-			echo "key-direction 1"
-			echo "<tls-auth>"
-			cat /etc/openvpn/tls-auth.key
-			echo "</tls-auth>"
-			;;
-		esac
-	} >>"$homeDir/$CLIENT.ovpn"
-
-	echo ""
-	echo "The configuration file has been written to $homeDir/$CLIENT.ovpn."
-	echo "Download the .ovpn file and import it in your OpenVPN client."
-
-	exit 0
-}
-
 function newMultipleClients() {
     echo ""
     echo "How many clients do you want to create?"
@@ -1192,6 +1093,7 @@ function newMultipleClients() {
             i=$((i-1)) # Reduce counter to retry
             CLIENT=""
         else
+            cd /etc/openvpn/easy-rsa/ || return
             case $PASS in
             1)
                 EASYRSA_CERT_EXPIRE=3650 ./easyrsa --batch build-client-full "$CLIENT" nopass
@@ -1201,8 +1103,33 @@ function newMultipleClients() {
                 EASYRSA_CERT_EXPIRE=3650 ./easyrsa --batch build-client-full "$CLIENT"
                 ;;
             esac
+            echo "Client $CLIENT added."
 
-            # Генерация OVPN файла для клиента
+            # Home directory of the user, where the client configuration will be written
+            if [ -e "/home/${CLIENT}" ]; then
+                # if $1 is a user name
+                homeDir="/home/${CLIENT}"
+            elif [ "${SUDO_USER}" ]; then
+                # if not, use SUDO_USER
+                if [ "${SUDO_USER}" == "root" ]; then
+                    # If running sudo as root
+                    homeDir="/root"
+                else
+                    homeDir="/home/${SUDO_USER}"
+                fi
+            else
+                # if not SUDO_USER, use /root
+                homeDir="/root"
+            fi
+
+            # Determine if we use tls-auth or tls-crypt
+            if grep -qs "^tls-crypt" /etc/openvpn/server.conf; then
+                TLS_SIG="1"
+            elif grep -qs "^tls-auth" /etc/openvpn/server.conf; then
+                TLS_SIG="2"
+            fi
+
+            # Generates the custom client.ovpn
             cp /etc/openvpn/client-template.txt "$homeDir/$CLIENT.ovpn"
             {
                 echo "<ca>"
@@ -1217,26 +1144,33 @@ function newMultipleClients() {
                 cat "/etc/openvpn/easy-rsa/pki/private/$CLIENT.key"
                 echo "</key>"
 
-                if grep -qs "^tls-crypt" /etc/openvpn/server.conf; then
+                case $TLS_SIG in
+                1)
                     echo "<tls-crypt>"
                     cat /etc/openvpn/tls-crypt.key
                     echo "</tls-crypt>"
-                elif grep -qs "^tls-auth" /etc/openvpn/server.conf; then
+                    ;;
+                2)
                     echo "key-direction 1"
                     echo "<tls-auth>"
                     cat /etc/openvpn/tls-auth.key
                     echo "</tls-auth>"
-                fi
+                    ;;
+                esac
             } >>"$homeDir/$CLIENT.ovpn"
 
-            echo "Client $CLIENT added."
-            CLIENT="" # Reset the CLIENT variable for next iteration
+            echo ""
+            echo "The configuration file has been written to $homeDir/$CLIENT.ovpn."
+            echo "Download the .ovpn file and import it in your OpenVPN client."
         fi
+
+        CLIENT="" # Reset the CLIENT variable for next iteration
     done
 
     echo ""
     echo "All clients created successfully."
 }
+
 
 
 function revokeClient() {
